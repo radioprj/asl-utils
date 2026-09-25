9e8cd26fbe39809a3c17a8c55fdea48f136a1e1d
Waldek <sp2ong@gmail.com>
2026-09-25 19:46:24 +0200

    Use variable for NODE in localplay command

--- meteo-alerts/play-burze.sh
+++ meteo-alerts/play-burze.sh
@@ -5,5 +5,5 @@ NODE="123456"
 
 # odtwrazanie meteo alert jeśli plik istnieje
 if [ -f "/opt/asl-utils/meteo-alerts/status/burza.ulaw" ]; then
-  /usr/sbin/asterisk -rx "rpt localplay $NODE /opt/asl-utils/meteo-alerts/status/burza"
+  /usr/sbin/asterisk -rx "rpt localplay ${NODE} /opt/asl-utils/meteo-alerts/status/burza"
 fi
