4669efbbb9f6602737c2ccd273787905c6c0692a
Waldek <sp2ong@gmail.com>
2026-09-25 19:43:12 +0200

    Use variable syntax for NODE in play-alerts.sh

--- meteo-alerts/play-alerts.sh
+++ meteo-alerts/play-alerts.sh
@@ -5,5 +5,5 @@ NODE="123456"
 
 # play meteo alert jeśli plik istnieje
 if [ -f "/opt/asl-utils/meteo-alerts/status/meteo.ulaw" ];  then
-  /usr/sbin/asterisk -rx "rpt localplay $NODE /opt/asl-utils/meteo-alerts/status/meteo"
+  /usr/sbin/asterisk -rx "rpt localplay ${NODE} /opt/asl-utils/meteo-alerts/status/meteo"
 fi
